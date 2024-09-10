dd1437412bd9e1d5468c322cd73030703171d90e
Daniel Sampaio Campos <dansampaio977@gmail.com>
2024-09-10 17:23:59 -0300

    aula 1

---
+++ index.js
@@ -0,0 +1,19 @@
+/*arrays
+let metas = ["daniel", "alo"]
+
+console.log(metas[1] + "," + metas[0]);*/
+
+let meta = {
+    value: "ler um livro por mes",
+    checked: true
+}
+
+let metas = [
+    meta, 
+    {
+        value: "caminhar 20 minutos todos os dias",
+        checked: false        
+    }
+]
+
+console.log(metas[0].value)
